310063cc0531f9d89000023f89919a95e69fdb1a
Steen Thomassen <steen@thomassen.net>
2014-05-17 22:21:21 +0200

    include/falsk_positiv.php * tilføj flere...
    include/vislinks.php
    * match for flere typer links til danskefilm
    
    match_filmografi.php
    * udskriv kandidater for personer
    * en enkelt fejlrettelse

--- include/falsk_positiv.php
+++ include/falsk_positiv.php
@@ -7,4 +7,13 @@
     $falsk_positiv_titel["16026"]["Danmarks_filmhistorie"] = 0;
     $falsk_positiv_titel["138"]["Aalholm"] = 0;
     $falsk_positiv_titel["74204"]["Ulrik_Langen"] = 0;
+
+    # midlertidg - rettet
+    $falsk_positiv_person["118107"]["Vera_Esbøll"] = 0;
+    $falsk_positiv_person["129275"]["Johanne_Krum-Hunderup"] = 0;
+    $falsk_positiv_person["150373"]["Aage_Hertel"] = 0;
+
+    # faste
+    $falsk_positiv_person["114644"]["Fyrtårnet_og_Bivognen"] = 0;
+    $falsk_positiv_person["129181"]["Kørsel_med_grønlandske_Hunde"] = 0;
 ?>
