e5131ff76b17b773ed1fc12ff0c375a4842b49eb
tools.dawikitool <tools.dawikitool@labs-vmbuilder-precise.pmtpa.wmflabs>
2014-06-13 14:55:52 +0000

    include/rolle.php vis_navn.php vis_titel.php * flyt konv_rolle til include/rolle.php

---
+++ include/rolle.php
@@ -0,0 +1,20 @@
+<?
+$konv_rolletype["Actors"] = "skuespiller";
+$konv_rolletype["Cinematography"] = "foto";
+$konv_rolletype["Direction"] = "instruktion";
+$konv_rolletype["Editing"] = "klipning";
+$konv_rolletype["Electrical dept."] = "lys";
+$konv_rolletype["Makeup"] = "makeup"; 
+$konv_rolletype["Medvirkende"] = "medvirkende";
+$konv_rolletype["Music"] = "musik";
+$konv_rolletype["Production design"] = "produktionsdesign";
+$konv_rolletype["Production"] = "produktion";
+$konv_rolletype["Script"] = "manuscript";
+$konv_rolletype["Sound"] = "lyd";
+$konv_rolletype["Stemme"] = "stemme";
+$konv_rolletype["Stills"] = "stills";
+$konv_rolletype["Stunt"] = "Stunt";
+$konv_rolletype["Voiceover"] = "Speaker";
+$konv_rolletype["Voice"] = "stemme";
+$konv_rolletype["Wardrobe"] = "kostumer"; 
+?>
--- vis_titel.php
+++ vis_titel.php
@@ -6,6 +6,7 @@ include "password.php";
 include "tab/wiki_database_opsaet.php";
 include "include/vislinks.php";
 include "include/falsk_positiv.php";
+include "include/rolle.php";
 
 ###########################################################################
 
@@ -159,7 +160,7 @@ where page_id=el_from
 
 	$antal=0;
 	while ($row = $result->fetch_object()) {
-		if(!isset($falsk_positiv_titel["$nr"]["$row->page_title"])) {
+		if(!isset($falsk_positiv_person["$nr"]["$row->page_title"])) {
 			$note_titel["$antal"]=$row->page_title;
 			$url=$row->el_to;
 			$antal++;
@@ -170,7 +171,7 @@ where page_id=el_from
 		echo "<li>duplet";
 		foreach($note_titel as $cur_person) {
 			$wikiurl="https://da.wikipedia.org/wiki/" . urlencode(strtr($cur_person, ' ', '_'));
-			echo " - \$falsk_positiv_titel[\"$nr\"][\"$cur_person\"] = 0; <a href=\"$wikiurl\">$cur_person</a>";	
+			echo " - \$falsk_positiv_person[\"$nr\"][\"$cur_person\"] = 0; <a href=\"$wikiurl\">$cur_person</a>";	
 		}
 		echo " - <a href=\"" . $url . "\">" . $nr . "</a>\n";
 		return 0;
@@ -241,24 +242,6 @@ where rd_from = $rd_id";
 	echo htmlentities($navn, ENT_COMPAT, "UTF-8");
 	return 0;
 }
-
-###########################################################################
-
-$konv_rolletype["Script"] = "manuscript";
-$konv_rolletype["Direction"] = "instruktion";
-$konv_rolletype["Cinematography"] = "foto";
-$konv_rolletype["Editing"] = "klipning";
-$konv_rolletype["Music"] = "musik";
-$konv_rolletype["Production design"] = "produktionsdesign";
-$konv_rolletype["Actors"] = "skuespiller";
-$konv_rolletype["Stills"] = "stills";
-$konv_rolletype["Voice"] = "stemme";
-$konv_rolletype["Production"] = "produktion";
-$konv_rolletype["Electrical dept."] = "lys";
-$konv_rolletype["Wardrobe"] = "kostumer"; 
-$konv_rolletype["Makeup"] = "makeup"; 
-$konv_rolletype["Sound"] = "lyd";
-$konv_rolletype["Stunt"] = "Stunt";
 
 ###########################################################################
 
